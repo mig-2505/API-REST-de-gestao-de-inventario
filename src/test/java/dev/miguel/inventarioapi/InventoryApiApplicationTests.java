a680abd1d50be27b878a306894283661884fe36a
Miguel <miguelvanuccisteam@gmail.com>
2026-08-27 14:16:08 -0300

    chore: setup inicial do projeto com Spring Boot e dependencias base

---
+++ src/test/java/dev/miguel/inventarioapi/InventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.miguel.inventarioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
